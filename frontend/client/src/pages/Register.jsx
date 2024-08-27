da026c2538448afb9a3bc334d0169d46e44ef0a8
nicholasboyce <nicholasboyce@protonmail.com>
2024-08-27 19:58:03 -0400

    Added back and next buttons.

--- frontend/client/src/pages/Register.jsx
+++ frontend/client/src/pages/Register.jsx
@@ -1,8 +1,26 @@
 import styles from './Register.module.css';
 import { Link } from 'react-router-dom';
 import RegisterFormItem from '../components/RegisterFormItem';
+import { useMultistepForm } from '../useMultistepForm';
 
 const Register = () => {
+    const {
+        steps,
+        currentStepIndex,
+        step,
+        isFirstStep,
+        isLastStep,
+        back,
+        next 
+    } = useMultistepForm([  
+        <>
+            <RegisterFormItem id='username' text='Username' type='text' length={3} name='username' />
+            <RegisterFormItem id='password' text='Password' type='password' length={5} name='password' />
+        </>,
+        <>
+            <p>Second page</p>
+        </>
+    ]);
     const convertToJSON = (data) => {
         const json = {};
 
@@ -40,13 +58,19 @@ const Register = () => {
         <>
         <div role='presentation' className={styles.registerPage}>
             <div className={styles.formBlock} role='presentation'>
-                <Link to='/' className={styles.logo}>LinkCards</Link>
+                <div className={styles['form-top-wrapper']}>
+                    <Link to='/' className={styles.logo}>LinkCards</Link> 
+                    <div className='steps-number'>{currentStepIndex + 1} / {steps.length}</div>
+                </div>
                 <main className={styles.main}>
                     <form action="/api/users" method="post" noValidate='novalidate' className={styles.registerForm} onSubmit={validateForm}>
                         <h1 className={styles.formHeading}>Create your LinkCards account</h1>
-                        <RegisterFormItem id='username' text='Username' type='text' length={3} name='username' />
-                        <RegisterFormItem id='password' text='Password' type='password' length={5} name='password' />
-                        <button type="submit" className={styles.registerFormSubmit}>Create account</button>
+                        {step}
+                        <div className={styles["button-wrapper"]}>
+                            {!isFirstStep && <button type="button" className={styles.registerFormSubmit} onClick={back}>Back</button>}
+                            {!isLastStep && <button type="button" className={`${styles.registerFormSubmit} ${styles.next}`} onClick={next}>Next</button>}
+                            {isLastStep && <button type="submit" className={styles.registerFormSubmit}>Create account</button>}
+                        </div>
                     </form>
                     <p>Have an account? <Link to='/login' className={styles.loginLink}>Sign in</Link></p>
                 </main>
