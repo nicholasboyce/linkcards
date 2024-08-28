e1b8cd5063084257b111fcc6f362c6263ac88797
nicholasboyce <nicholasboyce@protonmail.com>
2024-08-28 11:40:14 -0400

    Imported REDIS_URL as environment variable.

--- server/utils/config.js
+++ server/utils/config.js
@@ -5,8 +5,11 @@ const MONGODB_URI = process.env.NODE_ENV === 'test' ?
     process.env.TEST_MONGODB_URI : 
     process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
+const REDIS_URL = process.env.REDIS_URL;
+
 module.exports = {
   MONGODB_URI,
   PORT,
-  SECRET
+  SECRET,
+  REDIS_URL
 }
