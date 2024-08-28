a60fc91f633a9d45a65eca3e3d923d22b2bb6d47
nicholasboyce <nicholasboyce@protonmail.com>
2024-08-27 22:25:43 -0400

    Successfully implemented new user creation from register page.

--- frontend/client/src/components/RegisterFormItem.jsx
+++ frontend/client/src/components/RegisterFormItem.jsx
@@ -1,15 +1,17 @@
 import { useRef, useState } from "react";
 import styles from './RegisterFormItem.module.css';
 
-const RegisterFormItem = ({ id, text, type, length, name }) => {
+const RegisterFormItem = ({ id, text, type, length, name, initial, updateFields }) => {
 
     const inputRef = useRef(null);
     const [valid, setValid] = useState(true);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const validateInput = () => {
+    const validateInput = (e) => {
         const input = inputRef.current;
         setValid(input.validity.valid);
+        // console.log({ name: e.target.value });
+        updateFields({ [name]: e.target.value });
         if (!input.validity.valid) {
             if (input.validity.valueMissing) {
                 setErrorMessage(`${text} is required`);
@@ -28,7 +30,7 @@ const RegisterFormItem = ({ id, text, type, length, name }) => {
     return (
         <p className={styles.registerFormItem}>
             <label htmlFor={id}>{text}</label>
-            <input ref={inputRef} type={type} id={id} name={name} aria-describedby='registerFormError' required onChange={validateInput} onInvalid={(e) => e.preventDefault()} minLength={length}/>
+            <input ref={inputRef} type={type} id={id} name={name} aria-describedby='registerFormError' required onChange={validateInput} onInvalid={(e) => e.preventDefault()} minLength={length} value={initial} />
             {!valid && <span className={styles.errorMessage} id='registerFormError' aria-live='polite'>{errorMessage}</span>}
         </p>
     );
--- frontend/client/src/pages/Register.jsx
+++ frontend/client/src/pages/Register.jsx
@@ -2,8 +2,27 @@ import styles from './Register.module.css';
 import { Link } from 'react-router-dom';
 import RegisterFormItem from '../components/RegisterFormItem';
 import { useMultistepForm } from '../useMultistepForm';
+import { useState } from 'react';
+
+const INITIAL_DATA = {
+    username: '',
+    password: '',
+    name: '',
+    location: '',
+    bio: '',
+    'link-1-name': '',
+    'link-2-name': '',
+    'link-1': '',
+    'link-2': ''
+};
 
 const Register = () => {
+    const [userData, setData] = useState(INITIAL_DATA);
+    const updateFields = (fields) => {
+        setData(prev => {
+            return {...prev, ...fields};
+        });
+    };
     const {
         steps,
         currentStepIndex,
@@ -14,17 +33,21 @@ const Register = () => {
         next 
     } = useMultistepForm([  
         <>
-            <RegisterFormItem id='username' text='Username' type='text' length={3} name='username' />
-            <RegisterFormItem id='password' text='Password' type='password' length={5} name='password' />
+            <RegisterFormItem id='username' text='Username' type='text' length={3} name='username' initial={userData.username} updateFields={updateFields} />
+            <RegisterFormItem id='password' text='Password' type='password' length={5} name='password' initial={userData.password} updateFields={updateFields} />
         </>,
         <>
-            <RegisterFormItem id='name' text='Name' type='text' length={3} name='name' />
-            <RegisterFormItem id='location' text='Location' type='text' length={3} name='location' />
-            <RegisterFormItem id='bio' text='Bio' type='text' length={3} name='bio' />
+            <RegisterFormItem id='name' text='Name' type='text' length={3} name='name' initial={userData.name} updateFields={updateFields}/>
+            <RegisterFormItem id='location' text='Location' type='text' length={3} name='location' initial={userData.location} updateFields={updateFields}/>
+            <RegisterFormItem id='bio' text='Bio' type='text' length={3} name='bio' initial={userData.bio} updateFields={updateFields}/>
         </>,
         <>
-            <RegisterFormItem id='link-1' text='Link #1' type='text' length={3} name='link-1' />
-            <RegisterFormItem id='link-2' text='Link #2' type='text' length={3} name='link-2' />
+            <RegisterFormItem id='link-1-name' text='Link #1 Name' type='text' length={3} name='link-1-name' updateFields={updateFields} initial={userData['link-1-name']}/>
+            <RegisterFormItem id='link-1' text='Link #1 URL' type='text' length={3} name='link-1' updateFields={updateFields} initial={userData['link-1']}/>
+        </>,
+        <>
+            <RegisterFormItem id='link-2-name' text='Link #2 Name' type='text' length={3} name='link-2-name' updateFields={updateFields} initial={userData['link-2-name']}/>
+            <RegisterFormItem id='link-2' text='Link #2' type='text' length={3} name='link-2' updateFields={updateFields} initial={userData['link-2']}/>
         </>
     ]);
     const convertToJSON = (data) => {
@@ -43,15 +66,37 @@ const Register = () => {
     const validateForm = async (e) => {
         e.preventDefault();
         const form = e.target;
-        const data = new FormData(form);
+        // const data = new FormData(form);
         
         if (form.reportValidity()) {
             if (isLastStep) {
+                const transferData = {
+                    username: userData.username,
+                    password: userData.password,
+                    data: {
+                        info: {
+                            name: userData.name,
+                            location: userData.location,
+                            bio: userData.bio
+                        },
+                        links: [
+                            {
+                                name: userData['link-1-name'],
+                                url: userData['link-1']
+                            }, 
+                            {
+                                name: userData['link-2-name'],
+                                url: userData['link-2']
+                            }
+                        ]
+                    }
+                };
+                // console.log(transferData);
                 const csrfResponse = await fetch('/api/csrf');
                 const csrf = await csrfResponse.json();
                 fetch('/api/users', {
                     method: 'POST',
-                    body: convertToJSON(data),
+                    body: JSON.stringify(transferData),
                     headers: {
                         'x-csrf-token': csrf.token,
                         'Content-Type': 'application/json'
