7be26164b23ba9d677d57033be0176594ce508c3
nicholasboyce <gh@nicholasboyce.dev>
2025-02-15 14:31:59 -0500

    Removed homepage redirect to example page.

--- frontend/client/src/pages/Home.jsx
+++ frontend/client/src/pages/Home.jsx
@@ -1,14 +1,9 @@
 import styles from './Home.module.css';
 import imgUrl from '../assets/product-screenshot.png';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
 const Home = () => {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/jessica1');
-    };
 
     return (
         <div className={styles.home} role="presentation">
@@ -19,7 +14,7 @@ const Home = () => {
                     <p className={styles.heroHeaderBody}>Make it easy for yourself to share all your socials in one place. One easy link for all your links!</p>
                     <Link to='/register' className={styles.button}>Register</Link>  
                 </hgroup>
-                <img className={styles.productImage} onClick={handleClick} src={imgUrl} alt="Example image of link card." />
+                <img className={styles.productImage} src={imgUrl} alt="Example image of link card." />
             </main>
         </div>
     )
